0e30bba568b0e6a87b301730268f7c5d810094ff
Jonathan28121111 <Jonathan120428@gmail.com>
2025-09-30 13:42:26 -0700

    Definimos que jugador va comenzar en la partida

--- lib/partida.dart
+++ lib/partida.dart
@@ -1,8 +1,18 @@
-enum Estado {creada,iniciada,finalizada}
+import 'dart:math';
+
+enum Estado {creada, iniciada, finalizada}
 
 class Partida {
-late Estado estado;
-Partida(){
-  estado = Estado.creada;
-}
+  late Estado estado;
+  late int quienva; 
+  
+  Partida() {
+    estado = Estado.creada;
+    int numeroAleatorio = Random().nextInt(100);
+    if (numeroAleatorio < 50) {
+      quienva = 1;
+    } else {
+      quienva = 2;
+    }
+  }
 }
--- test/partida_test.dart
+++ test/partida_test.dart
@@ -1,7 +1,11 @@
+import 'package:batallanaval/partida.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('partida ...', () async {
+  test('La partida se creó correctamente', () {
+    final partida = Partida();
     
+    expect(partida.estado, equals(Estado.creada));
+    expect(partida.quienva, isIn([1, 2]));
   });
 }
